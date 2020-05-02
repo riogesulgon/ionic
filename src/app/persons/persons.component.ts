2f8e7ec0b1b8c50105238c93f798c9642da8040b
riogesulgon <rio.g.gesulgon@gmail.com>
2020-05-03 00:03:48 +0800

    RxJs

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-persons',
     templateUrl: './persons.component.html'
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit,OnDestroy {
+  personSubscription: Subscription;
   personList: string[];
 
   constructor(private personService: PersonsService) {}
 
   ngOnInit(): void {
     this.personList = this.personService.personList;
+    this.personSubscription = this.personService.personsChanged.subscribe(persons => {
+      this.personList = persons;
+    })
   }
 
+  ngOnDestroy(): void {
+    this.personSubscription.unsubscribe();
+  }
+
+  onRemovePerson(person: string) {
+    this.personService.removePerson(person);
+  }
 }
