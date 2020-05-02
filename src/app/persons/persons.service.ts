6ae6f60185afc12b0657c5900bf6524cbdc452fd
riogesulgon <rio.g.gesulgon@gmail.com>
2020-05-03 00:18:09 +0800

    RxJs

--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject} from 'rxjs'
 
 @Injectable({
   providedIn: "root"
 })
 export class PersonsService {
+  personsChanged = new Subject<string[]>();
   personList = ['Max', 'Mervin', 'Mandy'];
+
   addPerson(name: string) {
     this.personList.push(name);
+    this.personsChanged.next(this.personList);
+  }
+
+  removePerson(name: string) {
+    this.personList = this.personList.filter(person => {
+      return (person !== name);
+    });
+    console.log(this.personList);
+    this.personsChanged.next(this.personList);
   }
 }
