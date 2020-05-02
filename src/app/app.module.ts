340cdc83a4c1432ff6418f1b0091058f292b78b6
Rio <rio.g.gesulgon@gmail.com>
2020-05-02 13:01:22 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PersonsComponent, PersonInputComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
